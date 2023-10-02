ab10fbb73da4f72dc2c6845be5e48b5854104793
JenkyYE <869386961@qq.com>
2023-10-03 00:26:05 +0800

    2

---
+++ assets/scroll.b6326979.js
@@ -0,0 +1,1 @@
+import{J as e,K as t}from"./index.084fccd6.js";function r(o){window.addEventListener("scroll",e(o,300)),t(()=>{window.removeEventListener("scroll",o)})}export{r as u};
---
+++ assets/scrollContentAnimation.aa1ac3ff.js
@@ -0,0 +1,1 @@
+import{u as i}from"./scroll.b6326979.js";function u(){i(l);function l(){o()}function o(){document.querySelectorAll(".js-scroll").forEach(n=>{t(n,100)&&c(n)})}function t(e,n=0){return e.getBoundingClientRect().top<=(window.innerHeight||document.documentElement.clientHeight)-n}function c(e){e.classList.add("scrolled")}}export{u};
